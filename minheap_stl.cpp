f36b599df0ab749882c4d6be7d79bff31dbcb575
upendra <upendrayadav8127@gmail.com>
2019-10-31 16:26:01 +0530

    maxHeap advance version added

--- minheap_stl.cpp
+++ minheap_stl.cpp
@@ -11,4 +11,15 @@ main()
         cout<<q.top().first;
         q.pop();
     }
-}
+    //Bsic version of Min Heap in c++ STL
+    priority_queue<int,vector<int>,greater<int>>q;
+     q.push(3); //pusing the value into the priority queque;
+     q.push(1);
+     q.push(10);
+      q.push(34);
+    while(!q.empty()){
+        cout<<q.top(); // max element ;
+        q.pop();
+        }
+
+    }
